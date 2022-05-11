5f806c517933bb0d4c9cf4082e82f140e7e77474
Harmit Sonani <harmitsonani@gmail.com>
2022-05-11 17:38:28 +0530

    Color paletee

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function generate(event){
-    var y = document.getElementById('textbox_id').value; 
-    if(y){
-        for(let i=0;i<y;i++){
-            var box = document.getElementById("box");
-            document.body.appendChild(box);
-            var div = document.createElement('div');
-            div.setAttribute("class","square")
-            div.style.backgroundColor = "rgb("+(255-i*15)+",0,0)"
-            box.appendChild(div)  
-        }
-        var y = document.getElementById('textbox_id').value = "";        
-    } 
-    else
-    {
-        alert("Number Is Required")
-    } 
+    let box = document.getElementById("box");
+    $("#box").empty()
+    setTimeout(()=>{
+        var y = document.getElementById('textbox_id').value; 
+        if(y){
+            for(let i=0;i<y;i++){
+                var box = document.getElementById("box");
+                document.body.appendChild(box);
+                var div = document.createElement('div');
+                div.setAttribute("class","square")
+                div.style.backgroundColor = "rgb("+(255-i*15)+",0,0)"
+                box.appendChild(div)  
+            }
+            var y = document.getElementById('textbox_id').value = "";        
+        } 
+        else
+        {
+            alert("Number Is Required")
+        } 
+    },500)
 }
